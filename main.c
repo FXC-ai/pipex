1057703df6d66d747a8f3cc142e5490c26d5947b
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-03-07 18:40:46 +0100

    Safety Push

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fcoindre <fcoindre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 10:34:33 by fcoindre          #+#    #+#             */
-/*   Updated: 2023/03/03 14:57:47 by fcoindre         ###   ########.fr       */
+/*   Updated: 2023/03/07 17:44:07 by fcoindre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,6 +14,10 @@
 #include <unistd.h>
 #include <sys/wait.h>
 #include <stdio.h>
+#include <fcntl.h>
+
+
+
 
 
 
@@ -23,10 +27,17 @@ int main(int argc, char *argv[], char *env[])
     (void) env;
     pid_t pid;
     int wait_status;
-    
+    char *arguments[4];
+
+    int fd_infile;
+    int fd_outfile;
+
+    int access_check;
+
+
     int pipefd[2];
     int pipe_status;
-    char buf[1024];
+
 
     int result_execve;
 
@@ -38,6 +49,7 @@ int main(int argc, char *argv[], char *env[])
     }
     
     pipe_status = pipe(pipefd);
+    
     if (pipe_status == -1)
     {
         perror("pipe");
@@ -53,30 +65,68 @@ int main(int argc, char *argv[], char *env[])
     
     if (pid == 0)
     {
-        char *arguments[3];
-        arguments[0] = "/bin/ls";
-        arguments[1] = "-l";
+        
+        arguments[0] = "/bin/cat";
+        arguments[1] = "-e";
         arguments[2] = NULL;
+        arguments[3] = NULL;
+
 		close(pipefd[0]);
 
-        // Ici il fau
+        // Ici il faut open le infile et mettre dans le outfile le resultat
 
+        fd_infile = open("infile", O_RDONLY | O_CREAT, 0644);
 
-        dup2(pipefd[1], 1);
+        printf("fd_infile = %d\n", fd_infile);
+        // => pour ce processus le 1 ne redirige plus vers la sortie standard mais vers le pipe
+        
+        dup2(fd_infile,0);
+        dup2(pipefd[1], 1); 
+        // 0 => Entree standard
+        // 1 => Sortie standard
+        // 2 => Erreur standard
+        
+        
+        //on remplace le processus par un autre
         result_execve = execve(arguments[0], arguments, env);
 
         
-		//printf("Check write : %zd\n", write(pipefd[1], "test", 5));
+		// printf("Check write : %zd\n", write(pipefd[1], "test", 5));
 		close(pipefd[1]);
         return 0;
     }
     else
     {
-        wait(&wait_status);
+        waitpid(-1, &wait_status, 0);
+
+        arguments[0] = "/usr/bin/wc";
+        arguments[1] = "-w";
+        arguments[2] = NULL;
+        arguments[3] = NULL;
 
 		close(pipefd[1]);
-		printf("Check read : %zd\n", read(pipefd[0], buf, 15));
-		printf("Message recu du processus fils : %s", buf);
+
+        fd_outfile = open("outfile", O_WRONLY | O_CREAT,0644);
+
+
+        dup2(pipefd[0],0);
+        dup2(fd_outfile,1);
+        
+
+        access_check = access("/usr/bin/wc", F_OK);
+        //printf("access_check = %d\n", access_check);
+        
+		//printf("Check read : %d\n", check_read = read(pipefd[0], buf, 1023));
+        //buf[check_read] = '\0';
+		//printf("Contenu du pipefd[0] : \n%s", buf);
+        
+        
+
+
+        result_execve = execve(arguments[0], arguments, env);
+        
+        //printf("result_execve = %d", result_execve);
+        
 		close(pipefd[0]);
     }
 
