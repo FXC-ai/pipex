a7981cea12f9bbbd421efdd0a9350638362a705c
Francois-xavier Coindreau <fcoindre@c1r5s7.42lausanne.ch>
2023-03-08 15:27:43 +0100

    Gestion des arguments : ok | Pipe fonctionne | Gestion des erreur : KO | tester : a faire...

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: fcoindre <fcoindre@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/03 10:34:33 by fcoindre          #+#    #+#             */
-/*   Updated: 2023/03/07 21:10:02 by fcoindre         ###   ########.fr       */
+/*   Updated: 2023/03/08 15:25:23 by fcoindre         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,7 +16,7 @@
 #include <stdio.h>
 #include <fcntl.h>
 
-int cmd_exists(char *cmd, char *env[]);
+char *cmd_exists(char *cmd, char *env[]);
 void	ft_free_tabs(char **tab, int h);
 int size_tab(char **tab);
 
@@ -43,13 +43,11 @@ int size_tab(char **tab)
     return count;
 }
 
-int cmd_exists(char *cmd, char *env[])
+char **create_tab_paths(char *env[])
 {
-    int i;
-    char **paths_cmd;
-    char *paths;
-    char *path_to_test;
-    char *tmp;
+    int     i;
+    char    *paths;
+    char    **paths_tab;
 
     i = 0;
     while (env[i])
@@ -60,78 +58,127 @@ int cmd_exists(char *cmd, char *env[])
         }
         i++;
     }
-
-    
-    paths_cmd = ft_split(paths, ':');
+    paths_tab = ft_split(paths, ':');
     free(paths);
     paths = NULL;
+    return (paths_tab);
+}
 
+char *create_path_cmd(char *path, char *cmd)
+{
+    char *tmp;
+    char *path_cmd;
+
+    tmp = ft_strjoin(path, "/");
+    path_cmd = ft_strjoin(tmp, cmd);
+    free(tmp);
+    tmp = NULL;
+
+    return path_cmd;
+}
+
+char *cmd_exists(char *cmd, char *env[])
+{
+    int i;
+    char **tab_paths;    
+    char *path_to_test;
+
+    tab_paths = create_tab_paths(env);
 
     i = 0;
-    while (paths_cmd[i])
+    while (tab_paths[i])
     {
-        // A mettre dans une fontion externe char *create_path_cmd(char *cmd, char *env[]) ?
-        tmp = ft_strjoin(paths_cmd[i], "/");
-        path_to_test = ft_strjoin(tmp, cmd);
-        free(tmp);
-        tmp = NULL;
+        path_to_test = create_path_cmd(tab_paths[i], cmd);
         if (access(path_to_test, F_OK) == 0)
         {
-            printf("[%d] path %s : OK\n", i, path_to_test);
-            free(path_to_test);
-            path_to_test = NULL;
-            break;
+            ft_free_tabs(tab_paths, size_tab(tab_paths));
+            return path_to_test;
         }
         free(path_to_test);
         path_to_test = NULL;
         i++;
     }
-    
-    ft_free_tabs(paths_cmd, size_tab(paths_cmd));
-
-    return 4200000;
-
+    ft_free_tabs(tab_paths, size_tab(tab_paths));
+    return (NULL);
 }
 
+void    child_process(int *pipefd, char *argv[], char *env[])
+{
+
+    int fd_infile;
+    char *path_cmd1;
+    char **tab_cmd1;
+
+    tab_cmd1 = ft_split(argv[2], ' ');
+    path_cmd1 = cmd_exists(tab_cmd1[0], env);
+    fd_infile = open(argv[1], O_RDONLY | O_CREAT, 0644);
+
+	close(pipefd[0]);
+ 
+
+    dup2(fd_infile,0);
+    dup2(pipefd[1], 1); 
+    close(fd_infile);
+    // 0 => Entree standard
+    // 1 => Sortie standard
+    // 2 => Erreur standard
+        
+    if (execve(path_cmd1, tab_cmd1, env) == -1)
+    {
+        perror("child execve");
+        close(pipefd[1]);
+        exit(EXIT_FAILURE);
+    }
+
+    //printf("result_execve = %d\n", result_execve);
+        
+	close(pipefd[1]);
+    
+}
+
+void    parent_process(int *pipefd, char *argv[], char *env[])
+{
+    int fd_outfile;
+    char *path_cmd2;
+    char **tab_cmd2;
+
+    tab_cmd2 = ft_split(argv[3], ' ');
+    path_cmd2 = cmd_exists(tab_cmd2[0], env);
+    
 
 
+    fd_outfile = open(argv[4], O_WRONLY | O_CREAT, 0644);
+
+	close(pipefd[1]);
+    dup2(pipefd[0],0);
+    dup2(fd_outfile,1);
+    close(fd_outfile);
+    
+    if (execve(path_cmd2, tab_cmd2, env) == -1)
+    {
+        perror("parent execve");
+        close(pipefd[1]);
+        exit(EXIT_FAILURE);
+    }
+    
+    close(pipefd[0]);
+}
 
 int main(int argc, char *argv[], char *env[])
 {
-
-
-    cmd_exists("awk", env);
-    (void) argc;
-    (void) argv;
-
-    
-    /*
-    (void) env;
     pid_t pid;
     int wait_status;
-    char *arguments[4];
-
-    int fd_infile;
-    int fd_outfile;
-
-    int access_check;
-
-
     int pipefd[2];
     int pipe_status;
 
-
-    int result_execve;
-
-
     if (argc != 5)
     {
-        ft_printf("Invalid number of arguments");
+        ft_putstr_fd("pipex : Invalid number of arguments\n", 2);
+
         return (0);
     }
-    
+
     pipe_status = pipe(pipefd);
-    
     if (pipe_status == -1)
     {
         perror("pipe");
@@ -144,77 +191,16 @@ int main(int argc, char *argv[], char *env[])
         perror("fork");
         exit(EXIT_FAILURE);
     }
-    
+
     if (pid == 0)
     {
-        
-        arguments[0] = "/bin/cat";
-        arguments[1] = "-e";
-        arguments[2] = NULL;
-        arguments[3] = NULL;
-
-		close(pipefd[0]);
-
-        // Ici il faut open le infile et mettre dans le outfile le resultat
-
-        fd_infile = open("infile", O_RDONLY | O_CREAT, 0644);
-
-        printf("fd_infile = %d\n", fd_infile);
-        // => pour ce processus le 1 ne redirige plus vers la sortie standard mais vers le pipe
-        
-        dup2(fd_infile,0);
-        dup2(pipefd[1], 1); 
-        // 0 => Entree standard
-        // 1 => Sortie standard
-        // 2 => Erreur standard
-        
-        
-        //on remplace le processus par un autre
-        result_execve = execve(arguments[0], arguments, env);
-
-        
-		// printf("Check write : %zd\n", write(pipefd[1], "test", 5));
-		close(pipefd[1]);
-        return 0;
+        child_process(pipefd, argv, env);
     }
     else
     {
         waitpid(-1, &wait_status, 0);
-
-        arguments[0] = "/usr/bin/wc";
-        arguments[1] = "-w";
-        arguments[2] = NULL;
-        arguments[3] = NULL;
-
-		close(pipefd[1]);
-
-        fd_outfile = open("outfile", O_WRONLY | O_CREAT,0644);
-
-
-        dup2(pipefd[0],0);
-        dup2(fd_outfile,1);
-        
-
-        access_check = access("/usr/bin/wc", F_OK);
-        //printf("access_check = %d\n", access_check);
-        
-		//printf("Check read : %d\n", check_read = read(pipefd[0], buf, 1023));
-        //buf[check_read] = '\0';
-		//printf("Contenu du pipefd[0] : \n%s", buf);
-        
-        
-
-
-        result_execve = execve(arguments[0], arguments, env);
-        
-        //printf("result_execve = %d", result_execve);
-        
-		close(pipefd[0]);
+        parent_process(pipefd, argv, env);
     }
-
-    
-    */
-
     return 0;
 }
 
